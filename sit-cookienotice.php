bfa504c532c6f983c77a397552e6f926ba0d050d
dvorakja <G5q6%we4#1Ac>
2021-12-09 15:25:26 +0100

    First commit

--- sit-cookienotice.php
+++ sit-cookienotice.php
@@ -13,8 +13,18 @@ if ( !defined('SCN_PLUGIN_PATH') ) {
 
 // Assets
 add_action( 'wp_enqueue_scripts', 'scn_styles', 99 );
-
 function scn_styles() {
-    wp_enqueue_style( 'cookieconsent', SCN_PLUGIN_PATH . '/assets/cookieconsent.css', array(), '1.0.1' );
+    wp_enqueue_style( 'cookieconsent', SCN_PLUGIN_PATH . 'assets/cookieconsent.css' );
 }
 
+if ( ! function_exists( 'wp_body_open' ) ) {
+    function wp_body_open() {
+        do_action( 'wp_body_open' );
+    }
+}
+
+add_action('wp_body_open', 'scn_scripts');
+function scn_scripts() {
+    echo '<script defer src="' . SCN_PLUGIN_PATH . 'assets/cookieconsent.js"></script>';
+    echo '<script defer src="' . SCN_PLUGIN_PATH . 'config.js"></script>';
+}

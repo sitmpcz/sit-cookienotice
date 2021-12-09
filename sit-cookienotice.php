61639c0479ca7b8353f00079084586253742f56d
dvorakja <G5q6%we4#1Ac>
2021-12-09 14:40:58 +0100

    First commit

---
+++ sit-cookienotice.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: SIT cookienotice
+ * Description: Lepší adresy pro CPT a taxonomy
+ * Version: 1.0.0
+ * Author: SIT:Jaroslav Dvořák
+ **/
+
+// Cesta k pluginu
+if ( !defined('SCN_PLUGIN_PATH') ) {
+    define( 'SCN_PLUGIN_PATH', plugin_dir_url( __FILE__ ) );
+}

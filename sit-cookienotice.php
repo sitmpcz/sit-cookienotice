ca4663764b98e66e1b19528979359dea10eb2421
dvorakja <G5q6%we4#1Ac>
2021-12-09 15:09:24 +0100

    First commit

--- sit-cookienotice.php
+++ sit-cookienotice.php
@@ -11,4 +11,10 @@ if ( !defined('SCN_PLUGIN_PATH') ) {
     define( 'SCN_PLUGIN_PATH', plugin_dir_url( __FILE__ ) );
 }
 
-// Start
+// Assets
+add_action( 'wp_enqueue_scripts', 'scn_styles', 99 );
+
+function scn_styles() {
+    wp_enqueue_style( 'cookieconsent', SCN_PLUGIN_PATH . '/assets/cookieconsent.css' );
+}
+

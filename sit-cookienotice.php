27d13be68e0bb90f0ebfa38de9f22ae579ec3b7f
dvorakja@plzen.eu <zyE259XrtpR5>
2021-12-13 14:17:39 +0100

    Custom config in theme

--- sit-cookienotice.php
+++ sit-cookienotice.php
@@ -12,12 +12,24 @@ if ( !defined('SCN_PLUGIN_PATH') ) {
 }
 
 // Assets
+
+// Default CSS
 add_action( 'wp_enqueue_scripts', 'scn_styles', 99 );
 function scn_styles() {
     wp_enqueue_style( 'cookieconsent-css', SCN_PLUGIN_PATH . 'assets/cookieconsent.css' );
 }
+
+//Javascript
+// Check if custom config file exists
+if ( file_exists( get_template_directory_uri() . "/cookieconsent-config.js" ) ) {
+    $config_path = get_template_directory_uri() . "/cookieconsent-config.js";
+}
+else {
+    $config_path = SCN_PLUGIN_PATH . 'cookieconsent-config.js';
+}
+// Add this
 add_action('wp_footer', 'scn_add_scripts');
 function scn_add_scripts() {
     wp_enqueue_script( 'cookieconsent-js', SCN_PLUGIN_PATH . 'assets/cookieconsent.js' );
-    wp_enqueue_script( 'cookieconsent-config', SCN_PLUGIN_PATH . 'config.js' );
+    wp_enqueue_script( 'cookieconsent-config', SCN_PLUGIN_PATH . 'cookieconsent-config.js' );
 }

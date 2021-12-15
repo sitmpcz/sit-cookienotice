7405f4b90f0ca6b01752d8233fc247ec9aeb1fc7
dvorakja <zyE259XrtpR5>
2021-12-15 13:16:55 +0100

    Code editor JS

--- assets/code-editor.js
+++ assets/code-editor.js
@@ -2,7 +2,7 @@
 (function($){
     $(function(){
 
-        if( $('.code-editor-scn').length ) {
+        if( $(".code-editor-scn").length ) {
             var editorSettings = wp.codeEditor.defaultSettings ? _.clone( wp.codeEditor.defaultSettings ) : {};
             editorSettings.codemirror = _.extend(
                 {},
@@ -13,7 +13,7 @@
                     mode: 'javascript',
                 }
             );
-            var editor = wp.codeEditor.initialize( $('.code-editor-scn'), editorSettings );
+            var editor = wp.codeEditor.initialize( $(".code-editor-scn"), editorSettings );
         }
 
     });

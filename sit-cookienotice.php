26fa4dedef8123a5c53630530bfe12a9cfbb1066
dvorakja <zyE259XrtpR5>
2021-12-15 20:21:03 +0100

    Update

--- sit-cookienotice.php
+++ sit-cookienotice.php
@@ -24,7 +24,6 @@ function scn_styles() {
 //Javascript
 
 // Prepare file path to check
-
 $location = get_template_directory_uri();
 $location = str_replace( "http://", "", $location );
 $location = str_replace( "https://", "", $location );
@@ -33,9 +32,11 @@ $location = $_SERVER['DOCUMENT_ROOT'] . $location;
 $filename = $location . "/cookie-config.js";
 
 // Check if custom config file exists
+// Mame vygenerovany JS config z ulozenych dat v admin?
 if ( file_exists( $filename ) ) {
     $config_path = get_template_directory_uri() . "/cookie-config.js";
 }
+// Pokud ne, vlozime nejaky zaklad
 else {
     $config_path = SCN_PLUGIN_PATH . 'cookie-config.js';
 }
@@ -44,15 +45,20 @@ clearstatcache();
 // Add this
 add_action('wp_footer', function() use ( $config_path ) {
     $digits = 3;
+    // Anti cache
     $c = rand( pow( 10, $digits - 1 ), pow (10, $digits ) - 1 );
-    wp_enqueue_script( 'cookienotice-js', SCN_PLUGIN_PATH . 'assets/cookieconsent.js?c=' . $c );
-    wp_enqueue_script( 'cookienotice-config', $config_path );
+    // Vendor
+    wp_enqueue_script( 'cookienotice-js', SCN_PLUGIN_PATH . 'assets/cookieconsent.js' );
+    // Config
+    wp_enqueue_script( 'cookienotice-config', $config_path . '?c=' . $c );
 } );
+
+// Ulozene JS sledovaci kody vlozime tam, kam patri :)
 
 // Add JS to head
 add_action( "wp_head", function() {
     $scn_head = get_option("scn_head");
-    if ( $scn_head ) {
+    if ( $scn_head && !current_user_can( 'administrator' ) && !current_user_can( 'editor' ) ) {
         echo wp_unslash( $scn_head );
     }
 }, 99, 0 );
@@ -60,12 +66,12 @@ add_action( "wp_head", function() {
 // Add JS to footer
 add_action( "wp_footer", function() {
     $scn_footer= get_option("scn_footer");
-    if ( $scn_footer ) {
+    if ( $scn_footer && !current_user_can( 'administrator' ) && !current_user_can( 'editor' ) ) {
         echo wp_unslash( $scn_footer );
     }
 }, 99, 0 );
 
-// Odkaz na stranku v admin
+// Odkaz na stranku naseho nastaveni v admin
 // Submenu Hlavniho nastaveni
 add_action( 'admin_menu', 'scn_add_admin_plugin_menu' );
 
@@ -91,36 +97,38 @@ function scn_register_plugin_settings(){
 
 }
 
-// After save options
-add_action( 'updated_option', function( $option_name, $old_value, $value ) use ( $cookie_config ) {
-
-        $scn_config = get_option("scn_config");
-        $config_js = ( $scn_config !== "" ) ? $scn_config : $cookie_config;
-
-        $location = get_template_directory_uri();
-        $location = str_replace( "http://", "", $location );
-        $location = str_replace( "https://", "", $location );
-        $location = str_replace( $_SERVER['HTTP_HOST'], "", $location );
-        $location = $_SERVER['DOCUMENT_ROOT'] . $location;
-        $filename = $location . "/cookie-config.js";
-
-        file_put_contents( $filename, $config_js );
-
-}, 10, 3 );
-
 // Stranka nastaveni pluginu
 function scn_add_admin_plugin_page(){
     require_once __DIR__ . "/views/admin-option-page.php";
 }
 
+// Ty textarea potrebujeme trochu chytrejsi :) Aby umeli editovat zdrojovy kod:
 // Code hightlights
 add_action( 'admin_enqueue_scripts', 'scn_add_page_scripts_enqueue_script' );
 
 function scn_add_page_scripts_enqueue_script( $hook ) {
-    global $post;
 
     if( 'settings_page_scn-settings' === $hook ) {
         wp_enqueue_code_editor( array( 'type' => 'text/html' ) );
         wp_enqueue_script( 'js-code-editor', SCN_PLUGIN_PATH . 'assets/code-editor.js', array( 'jquery' ), '', true );
+
     }
 }
+
+// Jakmile zmenime nastaveni v admin, vygenerujeme soubor cookie-config.js
+// After save options
+add_action( 'updated_option', function( $option_name, $old_value, $value ) use ( $cookie_config ) {
+
+    $scn_config = get_option("scn_config");
+    $config_js = ( $scn_config !== "" ) ? $scn_config : $cookie_config;
+
+    $location = get_template_directory_uri();
+    $location = str_replace( "http://", "", $location );
+    $location = str_replace( "https://", "", $location );
+    $location = str_replace( $_SERVER['HTTP_HOST'], "", $location );
+    $location = $_SERVER['DOCUMENT_ROOT'] . $location;
+    $filename = $location . "/cookie-config.js";
+
+    file_put_contents( $filename, $config_js );
+
+}, 10, 3 );

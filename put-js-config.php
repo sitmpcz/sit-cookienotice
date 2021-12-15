f045c8c2855277429e42fa59facd762195f92f33
dvorakja <zyE259XrtpR5>
2021-12-15 23:01:56 +0100

    Update

--- put-js-config.php
+++ put-js-config.php
@@ -3,7 +3,7 @@
 function scn_put_js_config( $config = "" ) {
 
     $scn_config = get_option("scn_config");
-    $config_js = ( $scn_config !== "" ) ? $scn_config : $config;
+    $config_js = ( $scn_config ) ? : $config;
 
     $location = get_template_directory_uri();
     $location = str_replace( "http://", "", $location );
